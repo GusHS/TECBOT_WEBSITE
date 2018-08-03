a4b07503f0291112071f8fdeedce2f651b6d3c77
GusHS <gustavohdzs@hotmail.com>
2018-08-03 15:32:18 -0500

    deleted docs, finished our story section

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ $(window).scroll(function(){
         $("#navbar").removeClass("bg-inverse");
     }
 });
+
+jQuery(document).ready(function(){
+    jQuery('#btn-1').click(function(){
+        jQuery('#taStory').val("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+    });
+
+    jQuery('#btn-2').click(function(){
+        jQuery('#taStory').val("Text 2");
+    });
+
+    jQuery('#btn-3').click(function(){
+        jQuery('#taStory').val("Text 3");
+    });
+});
