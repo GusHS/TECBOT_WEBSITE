26027d10b13810c1d63e786d075357a8cc21682d
GusHS <gustavohdzs@hotmail.com>
2018-08-04 13:21:37 -0500

    finished our story section

--- js/script.js
+++ js/script.js
@@ -14,24 +14,32 @@ request.open('GET', requestURL); // Opens new request, use HTTP 'GET' method to 
 request.responseType = 'json';
 request.send(); 
 
-request.onload = function (){
-    var jStories = request.response;
+request.onload = function (){ 
+    var jStories = request.response; 
     setInitialText(jStories);
 
     jQuery(document).ready(function(){
         jQuery('#btn-1').click(function(){
             setInitialText(jStories);
+            jQuery('#btn-1').addClass('active');
+            jQuery('#btn-2').removeClass('active');
+            jQuery('#btn-3').removeClass('active');
         });
     
-        jQuery('#btn-2').click(function(){  
+        jQuery('#btn-2').click(function(){
             var text = jStories['stories'][1];
             jQuery('#taStory').val(text);
+            jQuery('#btn-2').addClass('active');
+            jQuery('#btn-1').removeClass('active');
+            jQuery('#btn-3').removeClass('active');
         });
     
         jQuery('#btn-3').click(function(){
-            
             var text = jStories['stories'][2];
             jQuery('#taStory').val(text);
+            jQuery('#btn-3').addClass('active');
+            jQuery('#btn-1').removeClass('active');
+            jQuery('#btn-2').removeClass('active');
         });
     });
 
