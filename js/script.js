906438656bb11ef81a32a28d7571c6cce75e5fea
GusHS <gustavohdzs@hotmail.com>
2018-08-04 11:43:24 -0500

    added json file

--- js/script.js
+++ js/script.js
@@ -6,10 +6,21 @@ $(window).scroll(function(){
         $("#navbar").removeClass("bg-inverse");
     }
 });
+ /** Changing Story Text**/
+ var requestURL = "../json/texts.json";
 
+
+window.onload = setInitialText;
+
+function setInitialText() {
+    //jQuery('#taStory').val("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+}
+
+/* JQuery implementation
 jQuery(document).ready(function(){
     jQuery('#btn-1').click(function(){
-        jQuery('#taStory').val("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+        setInitialText();
     });
 
     jQuery('#btn-2').click(function(){
@@ -20,3 +31,4 @@ jQuery(document).ready(function(){
         jQuery('#taStory').val("Text 3");
     });
 });
+*/
